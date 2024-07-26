e4afbf61346afe03d7ac80a194ec89091b5a96fd
factorgh <burchellsbale@gmail.com>
2024-07-26 00:34:03 +0000

    set dio interceptor

---
+++ lib/classes/dio_singleton.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DioSingleton {
+  static final DioSingleton _singleton = DioSingleton._internal();
+  late Dio dio;
+
+  factory DioSingleton() {
+    return _singleton;
+  }
+
+  DioSingleton._internal() {
+    _initializeDio();
+  }
+
+  Future<void> _initializeDio() async {
+    final prefs = await SharedPreferences.getInstance();
+    dio = Dio(BaseOptions(
+      baseUrl: 'https://fitness.adroit360.com/api/v1', 
+    ));
+    dio.interceptors.add(AuthInterceptor(prefs));
+  }
+}
+
+class AuthInterceptor extends Interceptor {
+  final SharedPreferences prefs;
+
+  AuthInterceptor(this.prefs);
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    final token = prefs.getString('auth_token');
+    if (token != null) {
+      options.headers['Authorization'] = $token;
+    }
+    handler.next(options);
+  }
+}
