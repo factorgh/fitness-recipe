cbd8fa39b86bfc49c04c63fc77678616d65b1dbb
factorgh <burchellsbale@gmail.com>
2024-07-30 10:45:53 +0000

    advert slider implemented

--- lib/screens/trainee_landing_screen.dart
+++ lib/screens/trainee_landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voltican_fitness/widgets/recipe_advert_slider.dart';
 import 'package:voltican_fitness/widgets/todays_pick.dart';
 import 'package:voltican_fitness/widgets/trainers_slider.dart';
 
@@ -82,169 +83,196 @@ class _TraineeLandingScreenState extends State<TraineeLandingScreen> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            // Profile section
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  RichText(
-                    text: const TextSpan(
-                      text: 'Hello ',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: ',Jennifer!',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.orangeAccent)),
-                      ],
-                    ),
-                  ),
-
-                  // right side of row
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(Icons.notifications, size: 15),
-                    ),
-                  )
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Profile section
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            // End of first row
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Stack(
-                children: [
-                  Container(
-                    height: 160,
-                    width: 360,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: const DecorationImage(
-                          image: AssetImage(
-                            "assets/recipe.jpg",
-                          ),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                  Positioned(
-                      top: 20,
-                      left: 20,
-                      child: Row(
-                        children: [
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Get Your Meal Today',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Text(
-                                'Easier With AR',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              Text(
-                                'Food Scanner',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            width: 40,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.orangeAccent),
-                            child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: [
-                                    Icon(Icons.qr_code_scanner_outlined),
-                                    SizedBox(width: 3),
-                                    Text("Scan Now")
-                                  ],
-                                )),
-                          ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: const TextSpan(
+                        text: 'Hello ',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: ',Jennifer!',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.orangeAccent)),
                         ],
-                      ))
-                ],
-              ),
-            ),
+                      ),
+                    ),
 
-            // End of second row
+                    // right side of row
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.notifications, size: 15),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // End of first row
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 160,
+                      width: 360,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                            image: AssetImage(
+                              "assets/recipe.jpg",
+                            ),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Positioned(
+                        top: 20,
+                        left: 20,
+                        child: Row(
+                          children: [
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Get Your Meal Today',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Text(
+                                  'Easier With AR',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                Text(
+                                  'Food Scanner',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 40,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.orangeAccent),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.qr_code_scanner_outlined),
+                                      SizedBox(width: 3),
+                                      Text("Scan Now")
+                                    ],
+                                  )),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+              ),
 
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Today's pick",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                  ),
-                  Text(
-                    "See All",
-                    style: TextStyle(
-                        color: Colors.red[600], fontWeight: FontWeight.w500),
-                  )
-                ],
+              // End of second row
+
+              const SizedBox(
+                height: 15,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TodaysPickSlider(
-                recipes: meals, onCategorySelected: handleCategorySelected),
-            const SizedBox(
-              height: 20,
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Top Trainers",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Today's pick",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          color: Colors.red[600], fontWeight: FontWeight.w500),
+                    )
+                  ],
+                ),
               ),
-            ),
-            // Trainers
-            TopTrainerSlider(
-              recipes: trainers,
-              onTrainerSelected: handleTrainerSelected,
-              images: images,
-            ),
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              TodaysPickSlider(
+                  recipes: meals, onCategorySelected: handleCategorySelected),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Top Trainers",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+              // Trainers
+
+              TopTrainerSlider(
+                recipes: trainers,
+                onTrainerSelected: handleTrainerSelected,
+                images: images,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Latest Nutritional News",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.brown),
+                    ),
+                  ],
+                ),
+              ),
+              const RecipeSlider(),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/recipe_advert_slider.dart
+++ lib/widgets/recipe_advert_slider.dart
@@ -1,1 +1,141 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
+class RecipeSlider extends StatefulWidget {
+  const RecipeSlider({super.key});
+
+  @override
+  _RecipeSliderState createState() => _RecipeSliderState();
+}
+
+class _RecipeSliderState extends State<RecipeSlider> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  late Timer _timer;
+
+  final List<Map<String, String>> _recipes = [
+    {
+      'title': 'Recipe 1',
+      'description': 'This is a description of Recipe 1.',
+      'imageUrl':
+          'https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg?auto=compress&cs=tinysrgb&w=800'
+    },
+    {
+      'title': 'Recipe 2',
+      'description': 'This is a description of Recipe 2.',
+      'imageUrl':
+          'https://images.pexels.com/photos/5966431/pexels-photo-5966431.jpeg?auto=compress&cs=tinysrgb&w=800'
+    },
+    {
+      'title': 'Recipe 3',
+      'description': 'This is a description of Recipe 3.',
+      'imageUrl':
+          'https://images.pexels.com/photos/6287493/pexels-photo-6287493.jpeg?auto=compress&cs=tinysrgb&w=800'
+    },
+    {
+      'title': 'Recipe 4',
+      'description': 'This is a description of Recipe 3.',
+      'imageUrl':
+          'https://images.pexels.com/photos/6210876/pexels-photo-6210876.jpeg?auto=compress&cs=tinysrgb&w=800'
+    },
+    {
+      'title': 'Recipe 5',
+      'description': 'This is a description of Recipe 3.',
+      'imageUrl':
+          'https://images.pexels.com/photos/5946643/pexels-photo-5946643.jpeg?auto=compress&cs=tinysrgb&w=800'
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _startAutoSlide();
+  }
+
+  void _startAutoSlide() {
+    _timer = Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < _recipes.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      _pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: _recipes.length,
+        itemBuilder: (context, index) {
+          return _buildRecipeCard(_recipes[index]);
+        },
+      ),
+    );
+  }
+
+  Widget _buildRecipeCard(Map<String, String> recipe) {
+    return Stack(
+      children: [
+        CachedNetworkImage(
+          imageUrl: recipe['imageUrl']!,
+          placeholder: (context, url) => const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black.withOpacity(0.7), Colors.transparent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                recipe['title']!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                recipe['description']!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
